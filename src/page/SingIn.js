e7994734ac6c70a5808c57b486206cdb359f5d0d
abdelhadi22k <115563437+abdelhadi22k@users.noreply.github.com>
2024-08-29 15:44:57 +0100

    Update SingIn.js

--- src/page/SingIn.js
+++ src/page/SingIn.js
@@ -1,44 +1,64 @@
 import Container from "react-bootstrap/Container";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux"; // لا تنس استخدام useDispatch
 import { userSignin } from "../redux/user/UserAction";
 import { Helmet } from "react-helmet-async";
 import domain from "../utils/config";
-
-
 function SingIn() {
   const navigate = useNavigate();
+  const dispatch = useDispatch(); // استخدم useDispatch لتحديث الـRedux
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [err, setErr] = useState("");
 
   const handelsubmit = async (event) => {
     event.preventDefault();
-    const data = await fetch(`${domain}/api/users/signin`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
+    setIsSubmitting(true);
 
-    const datas = await data.json();
-    console.log(datas);
-    userSignin({
-      type: "USER_SIGNIN",
-      payload: data,
-    });
-    localStorage.setItem("userInfo", JSON.stringify(data));
-    navigate("/");
+    try {
+      const response = await fetch(`${domain}/api/users/signin`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const datas = await response.json();
+      if (response.ok) {
+        const { token, user } = datas;
+        dispatch(
+          userSignin({
+            type: "USER_SIGNIN",
+            payload: { token, user },
+          })
+        );
+        localStorage.setItem("userInfo", JSON.stringify({ token, user }));
+        setTimeout(() => {
+          navigate("/");
+          window.location.reload(); 
+        }, 1000);
+      } else {
+        setErr(datas.message);
+      }
+    } catch (error) {
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
-    <Container className="Container">
+    <Container className="signIn-container">
       <Helmet>
-        <title> Sign In</title>
+        <title>Sign In</title>
       </Helmet>
-      <div className="mainContainers">
-        <div className="input">
+
+      <div className="form-wrapper">
+        <h2 className="form-title">Sign In</h2>
+
+        <div className="form-group">
           <label htmlFor="email">Email</label>
           <input
             id="email"
@@ -50,7 +70,7 @@ function SingIn() {
           />
         </div>
 
-        <div className="input">
+        <div className="form-group">
           <label htmlFor="password">Password</label>
           <input
             id="password"
@@ -62,15 +82,20 @@ function SingIn() {
           />
         </div>
 
-        <div className="submit">
-          <button type="submit" className="btnsub" onClick={handelsubmit}>
-            Sign In
+        <div className="form-actions">
+          <button
+            type="submit"
+            className="submit-button"
+            onClick={handelsubmit}
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Signing in..." : "Sign In"}
           </button>
 
-          <Link className="naveget" to={"/singup"}>
-            {" "}
-            You don't have account{" "}
+          <Link className="sign-up-link" to={"/singup"}>
+            Don't have an account? Sign Up
           </Link>
+          <span className="sign-up-link-err">{err}</span>
         </div>
       </div>
     </Container>
